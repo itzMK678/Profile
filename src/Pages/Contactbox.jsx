28f61f090bd3c245ef03b264c8700eff7c757db2
ItzMK <raomamoon592@gmail.com>
2025-07-12 03:40:52 +0500

    box3s responsibity

--- src/Pages/Contactbox.jsx
+++ src/Pages/Contactbox.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import "../components/Box.css"
-
+import "../components/Box.css";
 
 const Contactbox = () => {
-  return (  
-    <>
-    <div id="contact-me" style={{ marginTop:"30px",}}>
-      <div className="abo"  style={{ display: "flex" ,margin:"20px",gap: "10px",paddingTop:"20px"}} >
-        <div className="input-box" style={{marginTop:'20px' }}>
+  return (
+    <div id="contact-me">
+      <div className="abo">
+        <div className="input-box">
           <input type="text" placeholder="Name" />
           <input type="email" placeholder="Email" />
           <input type="number" placeholder="Phone Number" />
           <input type="text" placeholder="Subject" />
           <button>Send now</button>
         </div>
-        <div className="msg" >
-          <textarea id="msg" style={{ marginTop:'20px',}}placeholder="Message for me" rows="6" />
+        <div className="msg">
+          <textarea id="msg" placeholder="Message for me" rows="6" />
         </div>
       </div>
     </div>
-    </>
   );
 };
 
