2bf150375bcc765eb71183e87f2fc30624e0e00f
ItzMK <raomamoon592@gmail.com>
2026-07-27 18:36:42 +0500

    working contactbox

--- src/Pages/Contactbox.jsx
+++ src/Pages/Contactbox.jsx
@@ -1,23 +1,64 @@
-import React from 'react';
+import React , {useState} from 'react';
 import "../components/Box.css";
 
 const Contactbox = () => {
+    const [message, setMessage] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    subject: "",
+    messageText: "",
+  });
+
+  const handleChange = (e) => {
+    setMessage({
+      ...message,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const whatsappMessage = `
+*New Portfolio Inquiry*
+
+Name: ${message.name}
+Email: ${message.email}
+Phone: ${message.phone}
+Subject: ${message.subject}
+
+Message:
+${message.messageText}
+`;
+
+    window.open(
+      `https://wa.me/923326325661?text=${encodeURIComponent(whatsappMessage)}`,
+      "_blank"
+    );
+  };
+
   return (
    
-    <div id="contact-me">
-      <div className="abo">
+    <>
+      <form onSubmit={handleSubmit}>
+        <div id="contact-me">
+       <div className="abo">
         <div className="input-box">
-          <input type="text" placeholder="Name" />
-          <input type="email" placeholder="Email" />
-          <input type="number" placeholder="Phone Number" />
-          <input type="text" placeholder="Subject" />
+          <input name="name" type="text" placeholder="Name" onChange={handleChange} />
+          <input name="email" type="email" placeholder="Email" onChange={handleChange} />
+          <input name="phone" type="number" placeholder="Phone Number" onChange={handleChange} />
+          <input name="subject" type="text" placeholder="Subject" onChange={handleChange} />
           <button>Send now</button>
         </div>
         <div className="msg">
-          <textarea id="msg" placeholder="Message for me" rows="6" />
+          <textarea name="messageText" id="msg" placeholder="Message for me" rows="6" onChange={handleChange} />
         </div>
-      </div>
-    </div>
+        </div>
+         </div>
+        </form>
+      </>
+   
   );
 };
 
