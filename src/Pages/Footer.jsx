5a2b6c9366bab3001f14843d8d69ef1ecae73418
ItzMK <raomamoon592@gmail.com>
2026-07-28 17:32:20 +0500

    add new project and project section

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
+    <>
     <div style={{textAlign:"center",width:"100%",height:'35px',color:"white",backgroundColor:"#a432f0ff"}}><p style={{paddingTop:'10px'}}>Made with Pure Effeorts , Skill and Love <i class="fa-solid fa-heart"></i> </p></div>
+ </>
   )
 }
 
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,111 +1,195 @@
-import React from 'react'
-import BlogGen from "../assets/BlogGen.png"
-import Brickbreaker from "../assets/Brick-breaker.png"
-import CaraWeb from "../assets/caraweb.png"
-import MrAPI from "../assets/MrAPI.png"
-import Samsung from "../assets/Samsung.png"
-import Snakegame from "../assets/Snakegame.png"
-import Tourism from "../assets/Tourism.png"
-import Login from "../assets/Login.png"
-import Cars from "../assets/Cars.png"
-import Skills from "../assets/Skills.png"
-import Sahara from "../assets/Sahara.png"
-import "../components/Box.css"
-import ProjectsCard from '../components/ProjectsCard'
+import React, { useState } from "react";
+import BlogGen from "../assets/BlogGen.png";
+import Brickbreaker from "../assets/Brick-breaker.png";
+import CaraWeb from "../assets/caraweb.png";
+import MrAPI from "../assets/MrAPI.png";
+import Samsung from "../assets/Samsung.png";
+import Snakegame from "../assets/Snakegame.png";
+import Tourism from "../assets/Tourism.png";
+import Login from "../assets/Login.png";
+import Cars from "../assets/Cars.png";
+import Skills from "../assets/Skills.png";
+import Sahara from "../assets/Sahara.png";
+import Flappybird from "../assets/FlappyBird.png";
+import Luxe from "../assets/LuxeHotel.png";
+import McDonald from "../assets/McDonald.png";
+import Tour from "../assets/MkTours.png";
+import "../components/Box.css";
+import ProjectsCard from "../components/ProjectsCard";
 
+const Category = ["All", "Website", "Tools", "Game"];
 
-const ProjectData=[ 
+const ProjectData = [
      {
-         image: Skills ,
-        name: ' Skills Creative ',
-        Technologies:'NEXT JS , TAILWIND CSS ',
-        type: ' Website',
-        link: "https://skillcreatives-lemon.vercel.app/"
-    },
-     {
-         image: Cars ,
-        name: ' Cars Ralley ',
-        Technologies:'React , TAILWIND CSS ',
-        type: ' Website',
-        link: "https://cars-really.vercel.app/"
-    },
-     {
-         image: Sahara ,
-        name: ' Sahara Wellfare ',
-        Technologies:'React JS , TAILWIND CSS ',
-        type: ' Website',
-        link: "https://sahara-ecru.vercel.app/"
-    },
-    {
-         image: MrAPI,
-        name: ' Api Tester',
-        Technologies:'React JS , TAILWIND CSS , OPENAI API ',
-        type: 'Tools',
-        link: "https://api-tester-promax.vercel.app/"
-    },
-    {
-         image: BlogGen,
-        name: ' Blog-Gpt',
-        Technologies:'React JS , TAILWIND CSS , OPENAI API ',
-        type: ' Tool',
-        link: 'https://blog-gpt-omega.vercel.app/'
-    },{
-         image: CaraWeb,
-         name: ' E-commerce website',
-        type:' Website',
-        Technologies:' HTML , CSS ,  Js ',
-        link: 'https://itzmk678.github.io/ecommerce-web/'},
-
-    {
-        image: Login,
-        name: ' Login Page',
-        type:' Website',
-         Technologies:' HTML , CSS , Js ',
-        link: 'https://itzmk678.github.io/login-page/'
-    },
- {
-        image: Tourism,
-        name: ' Tourism Web',
-        type:' Website',
-         Technologies:' HTML , CSS ,  Js ',
-        link: 'https://itzmk678.github.io/tourist/'
-    },
-     {
-        image: Samsung,
-        name: ' Samsung Web',
-        type:' Website',
-        Technologies:' HTML , CSS , Js ',
-        link: 'https://itzmk678.github.io/samsung/'
-    },
-     {
-        image: Snakegame,
-        name: ' Snake',
-        Technologies:' HTML , CANVAS',
-        type: ' Game',
-        link: 'https://itzmk678.github.io/snake-game/'
-    },
-    {
-        image: Brickbreaker,
-        name: ' Brick-Breaker',
-        Technologies:' HTML , CANVAS',
-        type: ' Game',
-        link: 'http://itzmk678.github.io/brick-breaker/'
-    }
-]
-
-
+    image: Tour,
+    name: "Mk Tours",
+    Technologies: "Next JS , TAILWIND CSS,Node",
+    type: "Website",
+    link: "https://my-journey-wine.vercel.app/",
+  },
+  {
+    image: Skills,
+    name: "Skills Creative",
+    Technologies: "NEXT JS , TAILWIND CSS",
+    type: "Website",
+    link: "https://skillcreatives-lemon.vercel.app/",
+  },
+  {
+    image: Cars,
+    name: "Cars Rally",
+    Technologies: "React , TAILWIND CSS",
+    type: "Website",
+    link: "https://cars-really.vercel.app/",
+  },
+  {
+    image: Sahara,
+    name: "Sahara Welfare",
+    Technologies: "React JS , TAILWIND CSS",
+    type: "Website",
+    link: "https://sahara-ecru.vercel.app/",
+  },
+  {
+    image: MrAPI,
+    name: "API Tester",
+    Technologies: "React JS , TAILWIND CSS , OPENAI API",
+    type: "Tools",
+    link: "https://api-tester-promax.vercel.app/",
+  },
+  {
+    image: BlogGen,
+    name: "Blog-GPT",
+    Technologies: "React JS , TAILWIND CSS , OPENAI API",
+    type: "Tools",
+    link: "https://blog-gpt-omega.vercel.app/",
+  },
+  {
+    image: CaraWeb,
+    name: "E-commerce Website",
+    Technologies: "HTML , CSS , JS",
+    type: "Website",
+    link: "https://itzmk678.github.io/ecommerce-web/",
+  },
+  {
+    image: Login,
+    name: "Login Page",
+    Technologies: "HTML , CSS , JS",
+    type: "Website",
+    link: "https://itzmk678.github.io/login-page/",
+  },
+  {
+    image: Tourism,
+    name: "Tourism Website",
+    Technologies: "HTML , CSS , JS",
+    type: "Website",
+    link: "https://itzmk678.github.io/tourist/",
+  },
+  {
+    image: Samsung,
+    name: "Samsung Website",
+    Technologies: "HTML , CSS , JS",    
+    type: "Website",
+    link: "https://itzmk678.github.io/samsung/",
+  },
+  {
+    image: Snakegame,
+    name: "Snake Game",
+    Technologies: "HTML , CANVAS",
+    type: "Game",
+    link: "https://itzmk678.github.io/snake-game/",
+  },
+  {
+    image: Brickbreaker,
+    name: "Brick Breaker",
+    Technologies: "HTML , CANVAS",
+    type: "Game",
+    link: "https://itzmk678.github.io/brick-breaker/",
+  },
+   {
+    image: Flappybird,
+    name: "Flappy Bird",
+    Technologies: "HTML , CANVAS",
+    type: "Game",
+    link: "https://itzmk678.github.io/flappy-bird/",
+  }, {
+    image: Luxe,
+    name: "Luxe Hotel",
+    Technologies: "Next JS , TAILWIND CSS,Node",
+    type: "Website",
+    link: "https://luke-hotel-95dq.vercel.app//",
+  }, {
+    image: McDonald,
+    name: "McDonald's Website",
+    Technologies: "Html , CSS , JS",
+    type: "Website",
+    link: "https://itzmk678.github.io/mcdonald/",
+  },
+];
 
 const Projects = () => {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  // Filter projects
+  const filteredProjects =
+    selectedCategory === "All"
+      ? ProjectData
+      : ProjectData.filter(
+          (project) => project.type === selectedCategory
+        );
+
   return (
     <>
-   <p style={{color:"#a432f0ff"}}> Projects</p>
-    <div className="projects" style={{ display: "flex", flexWrap: "wrap", gap: "10px", width: "100%", justifyContent: "center" ,}}>
-   {ProjectData.map((item,index) =>(
-    <ProjectsCard key={index} image={item.image}  name={item.name} type={item.type} link={item.link} Technologies={item.Technologies}/>
-   ))}
-    </div>
-     </>
-  )
-}
+      <p style={{ color: "#a432f0ff" }}>Projects</p>
 
-export default Projects
+      {/* Category Buttons */}
+      <div>
+        {Category.map((item, index) => (
+          <div
+            key={index}
+            className="box"
+            style={{
+              display: "inline-block",
+              border: "0.5px solid rgb(138,0,230)",
+              width: "70px",
+              textAlign: "center",
+              margin: "10px",
+              padding: "10px",
+              borderRadius: "22px",
+              color: "#fff",
+              cursor: "pointer",
+              backgroundColor:
+                selectedCategory === item ? "#8a00e6" : "black",
+            }}
+            onClick={() => setSelectedCategory(item)}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+
+      {/* Projects */}
+      <div
+        className="projects"
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "10px",
+          width: "100%",
+          justifyContent: "center",
+        }}
+      >
+        {filteredProjects.map((item, index) => (
+          <ProjectsCard
+            key={index}
+            image={item.image}
+            name={item.name}
+            type={item.type}
+            link={item.link}
+            Technologies={item.Technologies}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Projects;
