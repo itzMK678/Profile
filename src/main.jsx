9974715fe9a66bc8593eeed3fa315cd99356739d
ItzMK <raomamoon592@gmail.com>
2026-08-02 14:43:00 +0500

    setting index

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
-import "./index.css";
+import "./Index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 <HelmetProvider>
